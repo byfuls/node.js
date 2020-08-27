dfcf18f5f0a7fe70c32dc5ba4788faf778f041fc
byfuls <byfuls@gmail.com>
2020-08-27 16:13:24 +0900

    (20200827,byfuls) regist functions trigger && simple test done

--- firestore/taste/trigger_function.js
+++ firestore/taste/trigger_function.js
@@ -1,30 +1,48 @@
-const sleep = require('sleep');
+'use strict';
 
-const serviceAccount = require('./cert/taste-firestore-f3cc55a6b2f1.json');
+//console.log(process.env)
+process.env.GCLOUD_PROJECT = "taste-firestore";
+process.env.FIREBASE_CONFIG = {
+	databaseURL: 'https://taste-firestore.firebaseio.com',
+	storageBucket: 'gs://taste-firestore.appspot.com',
+	projectId: 'taste-firestore'
+};
+
+const functions = require('firebase-functions');
+const serviceAccount = require('./cert/taste-firestore-a5ac9d55864e.json');
+//const adminConfig = JSON.parse(process.env.FIREBASE_CONFIG);
+//adminConfig.credential = admin.credential.cert(serverAccount);
 
 /*--- init, google cloud platform */
 const admin = require('firebase-admin');
+//admin.initializeApp(adminConfig);
+//admin.initializeApp();
 admin.initializeApp({
-  //credential: admin.credential.applicationDefault()
-  credential: admin.credential.cert(serviceAccount)
+	credential: admin.credential.cert(serviceAccount)
 });
 const db = admin.firestore();
 console.log(db)
 
 /*--- trigger init */
-const functions = require('firebase-functions');
+//const functions = require('firebase-functions');
+
+var wd = require('./functions/index.js');
 const trgr_one_doc = () => {
-	/* listen for any change on document 'alovelace' in collection 'users' */
-	//functions.firestore.document('users/alovelace').onWrite((change, context) => {
-	//	console.log('change!!!!!!!!!!!!!!!!!!!!!!!!!');
-	//});
+	wd.watchDoc;
 }
 
 
 
 /*--- main */
+const sleep = require('sleep');
 const main = () => {
+	console.log('start');
 	trgr_one_doc();
 	console.log('done');
+
+	while(true){
+		sleep.sleep(1);
+		console.log('ing');
+	}
 }
 main();
