c4f025b26f3a63dd178539bf083ec9b8cbbfa689
byfuls <byfuls@gmail.com>
2020-09-04 18:20:18 +0900

    (20200904,byfuls) sample with firestore

--- firestore/taste/snapshot.js
+++ firestore/taste/snapshot.js
@@ -1,68 +1,174 @@
-
+"use strict";
 const serviceAccount = require('./cert/taste-firestore-f3cc55a6b2f1.json');
-
-/*--- init, google cloud platform */
 const admin = require('firebase-admin');
 admin.initializeApp({
-  //credential: admin.credential.applicationDefault()
-  credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert(serviceAccount)
 });
-
 const db = admin.firestore();
 
-let conv_date = unix_timestamp => {
-	let date = new Date(unix_timestamp*1000);
-	let hours = date.getHours();
-	let minutes = "0"+date.getMinutes();
-	let seconds = "0"+date.getSeconds();
-	let formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
-	return formattedTime
-};
+var eventAgent = eventAgent || {}; 
+(function(_ea) {
+    let conv_date = unix_timestamp => {
+        let date = new Date(unix_timestamp*1000);
+        let hours = date.getHours();
+        let minutes = "0"+date.getMinutes();
+        let seconds = "0"+date.getSeconds();
+        let formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+        return formattedTime
+    };  
 
-/*--- get data via snapshot */
-var g_data = 0
-const sleep = require('sleep');
-let observer = db.collection('users').doc('test')
-	.onSnapshot(doc => {
-	console.log('event: ', doc);
-	console.log(`current data: ${doc.data()}`);
-	console.log(doc.data());
-
-	//j_doc = JSON.stringify(doc.data());
-	//console.log(j_doc);
-	j_doc = doc.data();
-	for(key in j_doc){
-		console.log(`[data] key: ${key}`);
-		console.log(`[data] value: ${j_doc[key]}`);
-		console.log(`[data] read time: ${conv_date(doc._readTime._seconds)}`);
-		console.log(`[data] create time: ${conv_date(doc._createTime._seconds)}`);
-		console.log(`[data] update time: ${conv_date(doc._updateTime._seconds)}`);
-		g_data++;
-	}
-
-	sleep.sleep(1);
-	let sender = "sender"+g_data;
-	let data = {
-	    sender: `hello, ${sender}`
+	let watch_doc = (_col, _doc) => {
+        let _observer = db.collection(_col).doc(_doc)
+        .onSnapshot(doc => {
+            let j_doc = doc.data();
+            for(key in j_doc){
+                console.log('------------ event occur ------------');
+                console.log(`[data] key         : ${key}`);
+                console.log(`[data] value       : ${j_doc[key]}`);
+                console.log(`[data] read time   : ${conv_date(doc._readTime._seconds)}`);
+                console.log(`[data] create time : ${conv_date(doc._createTime._seconds)}`);
+                console.log(`[data] update time : ${conv_date(doc._updateTime._seconds)}`);
+                console.log('-------------------------------------');
+            }   
+        }, err => {
+			console.error(`[err] error message: ${err}`);
+		}); 
 	};
-	const set_doc = db.collection('users').doc('test').set(data);
-	set_doc.then(res => {
-	    console.log('set: ', res);
-	});
-});
 
-/*--- get matched data via snapshot */
-//db.collection("users").where("aa", "==", "aa")
-//	.onSnapshot(function(snapshot) {
-//	snapshot.docChanges().forEach(function(change) {
-//		if (change.type === "added") {
-//			console.log("New: ", change.doc.data());
-//		}
-//		if (change.type === "modified") {
-//			console.log("Modified: ", change.doc.data());
-//		}
-//		if (change.type === "removed") {
-//			console.log("Removed: ", change.doc.data());
-//		}
-//	});
-//});
+	let watch_condition_docs = (_col, _if) => {
+		console.log(_if);
+		/*--- get matched data via snapshot */
+		db.collection(_col).where(_if[0], _if[1], _if[2])
+			.onSnapshot(function(snapshot) {
+			snapshot.docChanges().forEach(function(change) {
+				if (change.type === "added") {
+					console.log("New: ", change.doc.data());
+				}
+				if (change.type === "modified") {
+					console.log("Modified: ", change.doc.data());
+				}
+				if (change.type === "removed") {
+					console.log("Removed: ", change.doc.data());
+				}
+			});
+		});
+	};
+
+	let watch_col = (_col) => {
+		let _observer = db.collection(_col)
+		.onSnapshot(docs => {
+            let _docs = docs.forEach(doc => {
+				//console.log(doc);
+            	let j_doc = doc.data();
+            	for(key in j_doc){
+            	    console.log('------------ event occur ------------');
+            	    console.log(`[data] key         : ${key}`);
+            	    console.log(`[data] value       : ${j_doc[key]}`);
+            	    console.log(`[data] read time   : ${conv_date(doc._readTime._seconds)}`);
+            	    console.log(`[data] create time : ${conv_date(doc._createTime._seconds)}`);
+            	    console.log(`[data] update time : ${conv_date(doc._updateTime._seconds)}`);
+            	    console.log('-------------------------------------');
+            	}   
+			});
+		}, err => {
+			console.error(`[err] error message: ${err}`);
+		});
+	};
+
+	let delete_doc = (_col, _doc) => {
+		db.collection(_col).doc(_doc).delete().then(()=>{
+			console.log('document successfully delete!');
+		}).catch(err => {
+			console.error(`error removing document: ${err}`);
+		});
+	};
+
+	let update_event = (_col, _user, _friend, _message) => {
+		let query = db.collection(_col).doc(_user)
+		let _ret = query
+		.set({})
+		.then(()=>{
+
+			query = query.collection("chats").doc(_friend)
+			_ret = query
+			.set({})
+			.then(()=>{
+				query = query.collection("messages")
+				_ret = query.add({
+					content: _message.content,
+					date: _message.date,
+					type: _message.type,
+					writer: _message.writer
+				}).then(()=>{
+					console.log(`document added`);
+				}).catch(err=>{
+					console.error(`error adding document: ${error}`);
+				});
+			}).catch(err=>{
+				console.error(`error adding document: ${err}`);
+			});
+		}).catch(err=>{
+			console.error(`error adding document: ${error}`);
+		});
+	};
+
+	let watch_n_proceed = (_col, _dest_col) => {
+		let _observer = db.collection(_col)
+		.onSnapshot(docs => {
+            let _docs = docs.forEach(doc => {
+            	let j_doc = doc.data();
+				console.log(doc.id, '=>', j_doc);
+				console.log(`doc.sender => ${j_doc.sender}`);
+
+				let _col = _dest_col;
+				let _user = j_doc.sender;
+				let _friend = j_doc.receiver;
+				let _message = {
+					content: j_doc.content,
+					date: j_doc.date,
+					type: j_doc.type,
+					writer: j_doc.sender
+				};
+
+				console.log(_col);
+				console.log(_user);
+				console.log(_friend);
+				console.log(_message);
+				update_event(_col, _user, _friend, _message);
+				//delete_doc(_col, doc.id);
+			});
+		}, err => {
+			console.error(`[err] error message: ${err}`);
+		});
+	};
+
+	let get_docs = (_col) => {
+		let _docs = db.collection(_col).get().then(snapshot => {
+			snapshot.forEach(doc => {
+				console.log(doc.id, '=>', doc.data());
+				console.log(`doc.sender => ${doc.data().sender}`);
+			});
+		}).catch(err => {
+			console.log('error getting documents', err);
+		});
+	};
+
+    _ea.run = () => {
+		/* call watch document */
+		//watch_doc(_col, _doc);
+		/* call watch document with condition */
+		//watch_condition_docs(_col, ["aa", "==", "bb"]);
+		/* call watch collection */
+		//watch_col(_col);
+		/* call delete document */
+		//delete_doc(_col, _doc);
+
+		//update_event("sms", "120", "112", {content: "gogogo", date: "20200904", type: "create", writer: "120"});
+		//get_docs("event_queue");
+		
+		watch_n_proceed("event_queue", "sms");
+    };  
+})(eventAgent);
+eventAgent.run();
+
+/* ref : https://github.com/firebase/snippets-web/blob/af138b333956e2403f7a38af3814e3baff050aea/firestore/test.firestore.js#L705-L718 */
