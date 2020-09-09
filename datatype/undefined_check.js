44f451e7cbe534e098cb740cfe1982bfaa5b77d1
byfuls <byfuls@gmail.com>
2020-09-09 11:08:52 +0900

    (20200909,byfuls) undefined data check

---
+++ datatype/undefined_check.js
@@ -0,0 +1,23 @@
+const data1 = "123123";
+const data2 = '';
+const data3 = 123;
+let data4; 
+
+console.log('________________________________');
+console.log(`data1's data: ${data1}`);
+console.log(`data2's data: ${data2}`);
+console.log(`data3's data: ${data3}`);
+console.log(`data4's data: ${data4}`);
+console.log('________________________________');
+console.log(`data1's type: ${typeof(data1)}`);
+console.log(`data2's type: ${typeof(data2)}`);
+console.log(`data3's type: ${typeof(data3)}`);
+console.log(`data4's type: ${typeof(data4)}`);
+console.log('________________________________');
+
+if(typeof(data4) == 'undefined'){
+	console.log(`data4 is undefined`);
+}
+if(typeof(data5) == 'undefined'){
+	console.log(`data5 is undefined`);
+}
