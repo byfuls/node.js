ead71d018313cc6c61c7f4ce23bd70ab9507707a
byfuls <byfuls@gmail.com>
2020-09-09 15:34:48 +0900

    (20200909,byfuls) env taste

---
+++ env/env.js
@@ -0,0 +1,10 @@
+
+console.log('process info');
+console.log(process);
+
+console.log('process env info');
+console.log(process.env);
+
+console.log('process env specific info');
+console.log(process.env['ENV_TEST_PATH']);
+console.log(process.env.ENV_TEST_PATH);
