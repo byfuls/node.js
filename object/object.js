a700975d6851ab1f1f4afcce7e6454a5d81529b7
byfuls <byfuls@gmail.com>
2020-09-09 12:48:18 +0900

    (20200909,byfuls) object test

---
+++ object/object.js
@@ -0,0 +1,14 @@
+
+let obj = {};
+
+obj.sender = 'jason';
+obj.receiver = 'peter';
+
+console.log(obj);
+
+let obj1 = Object();
+
+obj1.sender = 'jason';
+obj1.receiver = 'peter';
+
+console.log(obj1);
